bad9657d3b5eef4a72b4dca2d8ac12e10b78e324
Anthony Attard <anthony@anthonyattard.com>
2022-05-06 02:05:38 +0000

    Add all functions

--- index.js
+++ index.js
@@ -2,4 +2,23 @@ function isAAA(string) {
   return string === "AAA";
 }
 
-module.exports = isAAA;
+function parseCity(string) {
+  var city = string.split(/,\s/).at(-2);
+  return city;
+}
+
+function parseState(string) {
+  let regexState =
+    /(A[KLRZ]|C[AOT]|D[CE]|FL|GA|HI|I[ADLN]|K[SY]|LA|M[ADEINOST]|N[CDEHJMVY]|O[HKR]|PA|RI|S[CD]|T[NX]|UT|V[AT]|W[AIVY])/g;
+  let state = string.match(regexState);
+  return state;
+}
+
+function parseZip(string) {
+  let regexZip = /\d{5}?/g;
+  var stateZip = string.split(/,\s/).at(-1);
+  let zip = stateZip.match(regexZip);
+  return zip;
+}
+
+module.exports = { isAAA, parseCity, parseState, parseZip };
