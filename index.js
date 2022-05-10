efbe131b60c1ca3d754e7ccff23bd754712ebb2c
Anthony Attard <anthony@anthonyattard.com>
2022-05-10 03:20:35 -0700

    Fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function parseState(string) {
   let regexState =
     /(A[KLRZ]|C[AOT]|D[CE]|FL|GA|HI|I[ADLN]|K[SY]|LA|M[ADEINOST]|N[CDEHJMVY]|O[HKR]|PA|RI|S[CD]|T[NX]|UT|V[AT]|W[AIVY])/g;
   let state = string.match(regexState);
-  return state;
+  return state[0];
 }
 
 function parseZip(string) {
@@ -20,7 +20,7 @@ function parseZip(string) {
   let arr = string.split(/,\s/);
   let stateZip = arr[arr.length - 1];
   let zip = stateZip.match(regexZip);
-  return zip;
+  return zip[0];
 }
 
 module.exports = { isAAA, parseCity, parseState, parseZip };
